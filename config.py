79345adc91d72ab4b4068b182f91b7de6e25e904
Ivan Gregor Tabalno <96023410+igmt-official@users.noreply.github.com>
2022-03-10 21:44:20 +0800

    Update config.py

--- config.py
+++ config.py
@@ -13,8 +13,7 @@ mod = "mod4"              # Sets mod key to SUPER/WINDOWS
 myTerm = "alacritty"      # My terminal of choice
 myBrowser = "chromium"    # My browser of choice
 
-def keys():
-    keys = [
+keys = [
          ### The essentials
          Key([mod], "Return",
              lazy.spawn(myTerm),
@@ -144,8 +143,7 @@ def keys():
          ])
 ]
 
-def layout():
-    layout_theme = {"border_width": 2,
+layout_theme = {"border_width": 2,
                 "margin": 20,
                 "border_focus": "f299a5",
                 "border_normal": "7e4f54"
@@ -200,59 +198,58 @@ def init_groups():
     return [Group(name, **kwargs) for name, kwargs in group_names]
 
 if __name__ in ["config", "__main__"]:
-    keys()
-    layout()
     
     group_names = init_groups()
     group = init_groups()
     
-    for i, (name, kwargs) in enumerate(group_names, 1):
+for i, (name, kwargs) in enumerate(group_names, 1):
     keys.append(Key([mod], str(i), lazy.group[name].toscreen())) # Swtich to another group
     keys.append(Key([mod, "shift"], str(i), lazy.window.togroup(name))) # Send current window to another group
     
-    # Drag floating layouts.
-    mouse = [
-    Drag([mod], "Button1", lazy.window.set_position_floating(), start=lazy.window.get_position()),
-    Drag([mod], "Button3", lazy.window.set_size_floating(), start=lazy.window.get_size()),
-    Click([mod], "Button2", lazy.window.bring_to_front()),
+# Drag floating layouts.
+mouse = [
+Drag([mod], "Button1", lazy.window.set_position_floating(), start=lazy.window.get_position()),
+Drag([mod], "Button3", lazy.window.set_size_floating(), start=lazy.window.get_size()),
+Click([mod], "Button2", lazy.window.bring_to_front()),
+]
+
+dgroups_key_binder = None
+dgroups_app_rules = []  # type: list
+follow_mouse_focus = True
+bring_front_click = False
+cursor_warp = False
+floating_layout = layout.Floating(
+    float_rules=[
+        # Run the utility of `xprop` to see the wm class and name of an X client.
+        *layout.Floating.default_float_rules,
+        Match(wm_class="confirmreset"),  # gitk
+        Match(wm_class="makebranch"),  # gitk
+        Match(wm_class="maketag"),  # gitk
+        Match(wm_class="ssh-askpass"),  # ssh-askpass
+        Match(title="branchdialog"),  # gitk
+        Match(title="pinentry"),  # GPG key password entry
     ]
+)
 
-    dgroups_key_binder = None
-    dgroups_app_rules = []  # type: list
-    follow_mouse_focus = True
-    bring_front_click = False
-    cursor_warp = False
-    floating_layout = layout.Floating(
-        float_rules=[
-            # Run the utility of `xprop` to see the wm class and name of an X client.
-            *layout.Floating.default_float_rules,
-            Match(wm_class="confirmreset"),  # gitk
-            Match(wm_class="makebranch"),  # gitk
-            Match(wm_class="maketag"),  # gitk
-            Match(wm_class="ssh-askpass"),  # ssh-askpass
-            Match(title="branchdialog"),  # gitk
-            Match(title="pinentry"),  # GPG key password entry
-        ]
-    )
-    auto_fullscreen = True
-    focus_on_window_activation = "smart"
-    reconfigure_screens = True
+auto_fullscreen = True
+focus_on_window_activation = "smart"
+reconfigure_screens = True
 
-    # If things like steam games want to auto-minimize themselves when losing
-    # focus, should we respect this or not?
-    auto_minimize = True
+# If things like steam games want to auto-minimize themselves when losing
+# focus, should we respect this or not?
+auto_minimize = True
 
-    @hook.subscribe.startup_once
-    def start_once():
-        home = os.path.expanduser('~')
-        subprocess.call([home + '/.config/qtile/autostart.sh'])
+@hook.subscribe.startup_once
+def start_once():
+    home = os.path.expanduser('~')
+    subprocess.call([home + '/.config/qtile/autostart.sh'])
 
-    # XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
-    # string besides java UI toolkits; you can see several discussions on the
-    # mailing lists, GitHub issues, and other WM documentation that suggest setting
-    # this string if your java app doesn't work correctly. We may as well just lie
-    # and say that we're a working one by default.
-    #
-    # We choose LG3D to maximize irony: it is a 3D non-reparenting WM written in
-    # java that happens to be on java's whitelist.
-    wmname = "LG3D"
+# XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
+# string besides java UI toolkits; you can see several discussions on the
+# mailing lists, GitHub issues, and other WM documentation that suggest setting
+# this string if your java app doesn't work correctly. We may as well just lie
+# and say that we're a working one by default.
+#
+# We choose LG3D to maximize irony: it is a 3D non-reparenting WM written in
+# java that happens to be on java's whitelist.
+wmname = "LG3D"
