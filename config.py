95d5d35212fcbba953e8d75c23f276bb6b21096d
Ivan Gregor Tabalno <96023410+igmt-official@users.noreply.github.com>
2022-03-10 20:27:38 +0800

    Added config file.

---
+++ config.py
@@ -0,0 +1,200 @@
+# -*- coding: utf-8 -*-
+import os
+import subprocess
+from libqtile import qtile
+from libqtile.config import Click, Drag, Group, KeyChord, Key, Match, Screen
+from libqtile.command import lazy
+from libqtile import layout, bar, widget, hook
+from libqtile.lazy import lazy
+from libqtile.utils import guess_terminal
+from typing import List  # noqa: F401from typing import List  # noqa: F401
+
+mod = "mod4"              # Sets mod key to SUPER/WINDOWS
+myTerm = "alacritty"      # My terminal of choice
+myBrowser = "qutebrowser" # My browser of choice
+
+keys = [
+         ### The essentials
+         Key([mod], "Return",
+             lazy.spawn(myTerm+" -e fish"),
+             desc='Launches My Terminal'
+             ),
+         Key([mod, "shift"], "Return",
+             lazy.spawn("dmenu_run -p 'Run: '"),
+             desc='Run Launcher'
+             ),
+         Key([mod], "b",
+             lazy.spawn(myBrowser),
+             desc='Qutebrowser'
+             ),
+         Key([mod], "Tab",
+             lazy.next_layout(),
+             desc='Toggle through layouts'
+             ),
+         Key([mod, "shift"], "c",
+             lazy.window.kill(),
+             desc='Kill active window'
+             ),
+         Key([mod, "shift"], "r",
+             lazy.restart(),
+             desc='Restart Qtile'
+             ),
+         Key([mod, "shift"], "q",
+             lazy.shutdown(),
+             desc='Shutdown Qtile'
+             ),
+         ### Window controls
+         Key([mod], "j",
+             lazy.layout.down(),
+             desc='Move focus down in current stack pane'
+             ),
+         Key([mod], "k",
+             lazy.layout.up(),
+             desc='Move focus up in current stack pane'
+             ),
+         Key([mod, "shift"], "j",
+             lazy.layout.shuffle_down(),
+             lazy.layout.section_down(),
+             desc='Move windows down in current stack'
+             ),
+         Key([mod, "shift"], "k",
+             lazy.layout.shuffle_up(),
+             lazy.layout.section_up(),
+             desc='Move windows up in current stack'
+             ),
+         Key([mod], "h",
+             lazy.layout.shrink(),
+             lazy.layout.decrease_nmaster(),
+             desc='Shrink window (MonadTall), decrease number in master pane (Tile)'
+             ),
+         Key([mod], "l",
+             lazy.layout.grow(),
+             lazy.layout.increase_nmaster(),
+             desc='Expand window (MonadTall), increase number in master pane (Tile)'
+             ),
+         Key([mod], "n",
+             lazy.layout.normalize(),
+             desc='normalize window size ratios'
+             ),
+         Key([mod], "m",
+             lazy.layout.maximize(),
+             desc='toggle window between minimum and maximum sizes'
+             ),
+         Key([mod, "shift"], "f",
+             lazy.window.toggle_floating(),
+             desc='toggle floating'
+             ),
+         Key([mod], "f",
+             lazy.window.toggle_fullscreen(),
+             desc='toggle fullscreen'
+             ),
+         ### Stack controls
+         Key([mod, "shift"], "Tab",
+             lazy.layout.rotate(),
+             lazy.layout.flip(),
+             desc='Switch which side main pane occupies (XmonadTall)'
+             ),
+          Key([mod], "space",
+             lazy.layout.next(),
+             desc='Switch window focus to other pane(s) of stack'
+             ),
+         Key([mod, "shift"], "space",
+             lazy.layout.toggle_split(),
+             desc='Toggle between split and unsplit sides of stack'
+             ),
+         # Dmenu scripts launched using the key chord SUPER+p followed by 'key'
+         KeyChord([mod], "p", [
+             Key([], "e",
+                 lazy.spawn("./dmscripts/dm-confedit"),
+                 desc='Choose a config file to edit'
+                 ),
+             Key([], "i",
+                 lazy.spawn("./dmscripts/dm-maim"),
+                 desc='Take screenshots via dmenu'
+                 ),
+             Key([], "k",
+                 lazy.spawn("./dmscripts/dm-kill"),
+                 desc='Kill processes via dmenu'
+                 ),
+             Key([], "l",
+                 lazy.spawn("./dmscripts/dm-logout"),
+                 desc='A logout menu'
+                 ),
+             Key([], "m",
+                 lazy.spawn("./dmscripts/dm-man"),
+                 desc='Search manpages in dmenu'
+                 ),
+             Key([], "o",
+                 lazy.spawn("./dmscripts/dm-bookman"),
+                 desc='Search your qutebrowser bookmarks and quickmarks'
+                 ),
+             Key([], "r",
+                 lazy.spawn("./dmscripts/dm-reddit"),
+                 desc='Search reddit via dmenu'
+                 ),
+             Key([], "s",
+                 lazy.spawn("./dmscripts/dm-websearch"),
+                 desc='Search various search engines via dmenu'
+                 ),
+             Key([], "p",
+                 lazy.spawn("passmenu"),
+                 desc='Retrieve passwords with dmenu'
+                 )
+         ])
+]
+
+layout_theme = {"border_width": 2,
+                "margin": 20,
+                "border_focus": "f299a5",
+                "border_normal": "7e4f54"
+                }
+
+layouts = [
+    layout.MonadTall(**layout_theme)
+]
+
+mouse = [
+    Drag([mod], "Button1", lazy.window.set_position_floating(),
+         start=lazy.window.get_position()),
+    Drag([mod], "Button3", lazy.window.set_size_floating(),
+         start=lazy.window.get_size()),
+    Click([mod], "Button2", lazy.window.bring_to_front())
+]
+
+dgroups_app_rules = []  # type: List
+follow_mouse_focus = True
+bring_front_click = False
+cursor_warp = False
+
+floating_layout = layout.Floating(float_rules=[
+    # Run the utility of `xprop` to see the wm class and name of an X client.
+    # default_float_rules include: utility, notification, toolbar, splash, dialog,
+    # file_progress, confirm, download and error.
+    *layout.Floating.default_float_rules,
+    Match(title='Confirmation'),      # tastyworks exit box
+    Match(title='Qalculate!'),        # qalculate-gtk
+    Match(wm_class='kdenlive'),       # kdenlive
+    Match(wm_class='pinentry-gtk-2'), # GPG key password entry
+])
+auto_fullscreen = True
+focus_on_window_activation = "smart"
+reconfigure_screens = True
+
+# If things like steam games want to auto-minimize themselves when losing
+# focus, should we respect this or not?
+auto_minimize = True
+
+@hook.subscribe.startup_once
+def start_once():
+    home = os.path.expanduser('~')
+    subprocess.call([home + '/.config/qtile/autostart.sh'])
+
+# XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
+# string besides java UI toolkits; you can see several discussions on the
+# mailing lists, GitHub issues, and other WM documentation that suggest setting
+# this string if your java app doesn't work correctly. We may as well just lie
+# and say that we're a working one by default.
+#
+# We choose LG3D to maximize irony: it is a 3D non-reparenting WM written in
+# java that happens to be on java's whitelist.
+wmname = "LG3D"
