c4d97f123256b116d2deaa9faf179328da427e9a
Ivan Gregor Tabalno <96023410+igmt-official@users.noreply.github.com>
2022-03-11 23:07:49 +0800

    Create screens.py

---
+++ settings/screens.py
@@ -0,0 +1,27 @@
+from libqtile import bar, widget
+from libqtile.config import Screen
+
+screens = [
+    Screen(
+        bottom=bar.Bar(
+            [
+                widget.CurrentLayout(),
+                widget.GroupBox(),
+                widget.Prompt(),
+                widget.WindowName(),
+                widget.Chord(
+                    chords_colors={
+                        "launch": ("#ff0000", "#ffffff"),
+                    },
+                    name_transform=lambda name: name.upper(),
+                ),
+                widget.TextBox("default config", name="default"),
+                widget.TextBox("Press &lt;M-r&gt; to spawn", foreground="#d75f5f"),
+                widget.Systray(),
+                widget.Clock(format="%Y-%m-%d %a %I:%M %p"),
+                widget.QuickExit(),
+            ],
+            24,
+        ),
+    ),
+]
