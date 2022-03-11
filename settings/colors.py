4bc06231da7fcd67a6830d35a12b20d84f5067a3
Ivan Gregor Tabalno <96023410+igmt-official@users.noreply.github.com>
2022-03-11 22:40:00 +0800

    Update colors.py

--- settings/colors.py
+++ settings/colors.py
@@ -1,5 +1,7 @@
-"bg" = "#f299a5"
-"darkBg" = "#e8849d"
-"fg" = "#ffffff"
-"grey" = "#7e4f54"
-"darkGrey" = "#4f3436"
+colors = {
+    "bg": "#f299a5"
+    "darkBg": "#e8849d"
+    "fg": "#ffffff"
+    "grey": "#7e4f54"
+    "darkGrey": "#4f3436"
+}
