258707cb25beb83cbe7827984e726bb23993c502
Ivan Gregor Tabalno <96023410+igmt-official@users.noreply.github.com>
2022-03-11 23:09:35 +0800

    Update config.py

--- config.py
+++ config.py
@@ -3,39 +3,12 @@ import subprocess
 
 from libqtile import hook
 
-from libqtile import bar, layout, widget
-from libqtile.lazy import lazy
-
 from settings.keys import mod, keys
 from settings.groups import groups
 from settings.layouts import layouts, floating_layout
 from settings.mouse import mouse
 from settings.widgets import widget_defaults, extension_defaults
-
-screens = [
-    Screen(
-        bottom=bar.Bar(
-            [
-                widget.CurrentLayout(),
-                widget.GroupBox(),
-                widget.Prompt(),
-                widget.WindowName(),
-                widget.Chord(
-                    chords_colors={
-                        "launch": ("#ff0000", "#ffffff"),
-                    },
-                    name_transform=lambda name: name.upper(),
-                ),
-                widget.TextBox("default config", name="default"),
-                widget.TextBox("Press &lt;M-r&gt; to spawn", foreground="#d75f5f"),
-                widget.Systray(),
-                widget.Clock(format="%Y-%m-%d %a %I:%M %p"),
-                widget.QuickExit(),
-            ],
-            24,
-        ),
-    ),
-]
+from settings.screens import screens
 
 dgroups_key_binder = None
 dgroups_app_rules = []  # type: list
