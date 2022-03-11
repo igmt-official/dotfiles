b3c7f191593e03770d87cee9efd62dd8713ba34c
Ivan Gregor Tabalno <96023410+igmt-official@users.noreply.github.com>
2022-03-11 18:49:03 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,198 +1,21 @@
 # -*- coding: utf-8 -*-
+# Main Modules
+# from libqtile.utils import guess_terminal
 import os
 import subprocess
-
-# Main Script
-from colors import zerotwo
-from ztbar import bar
-
+from typing import List 
 from libqtile import hook
 
-from libqtile.extension.dmenu import DmenuRun
-from libqtile.extension.window_list import WindowList
-from libqtile.extension.command_set import CommandSet
+# Separate Modules
+from settings.key import keys, mouse
+from settings.layouts import groups, layouts, widget_defaults, screens
 
-# Import widgets and bar
-from libqtile.config import Click, Drag, DropDown, Group, Key, Match, ScratchPad, Screen
-from libqtile.lazy import lazy
-
-# Import layout objects
-from libqtile.layout.columns import Columns
-from libqtile.layout.xmonad import MonadTall
-from libqtile.layout.stack import Stack
-from libqtile.layout.floating import Floating
-
-# from libqtile.utils import guess_terminal
-from typing import List 
-
-mod = "mod4"              # Sets mod key to SUPER/WINDOWS
-myTerm = "alacritty"      # My terminal of choice
-myBrowser = "qutebrowser"    # My browser of choice
-
-keys = [
-         ### The essentials
-         Key([mod], "Return",
-             lazy.spawn(myTerm),
-             desc='Launches My Terminal'
-             ),
-         Key([mod, "shift"], "Return",
-             lazy.spawn("dmenu_run -p 'Run: '"),
-             desc='Run Launcher'
-             ),
-         Key([mod], "b",
-             lazy.spawn(myBrowser),
-             desc='QuteBrowser'
-             ),
-         Key([mod], "Tab",
-             lazy.next_layout(),
-             desc='Toggle through layouts'
-             ),
-         Key([mod, "shift"], "c",
-             lazy.window.kill(),
-             desc='Kill active window'
-             ),
-         Key([mod, "shift"], "r",
-             lazy.restart(),
-             desc='Restart Qtile'
-             ),
-         Key([mod, "shift"], "q",
-             lazy.shutdown(),
-             desc='Shutdown Qtile'
-             ),
-         ### Window controls
-         Key([mod], "j",
-             lazy.layout.down(),
-             desc='Move focus down in current stack pane'
-             ),
-         Key([mod], "k",
-             lazy.layout.up(),
-             desc='Move focus up in current stack pane'
-             ),
-         Key([mod, "shift"], "j",
-             lazy.layout.shuffle_down(),
-             lazy.layout.section_down(),
-             desc='Move windows down in current stack'
-             ),
-         Key([mod, "shift"], "k",
-             lazy.layout.shuffle_up(),
-             lazy.layout.section_up(),
-             desc='Move windows up in current stack'
-             ),
-         Key([mod], "h",
-             lazy.layout.shrink(),
-             lazy.layout.decrease_nmaster(),
-             desc='Shrink window (MonadTall), decrease number in master pane (Tile)'
-             ),
-         Key([mod], "l",
-             lazy.layout.grow(),
-             lazy.layout.increase_nmaster(),
-             desc='Expand window (MonadTall), increase number in master pane (Tile)'
-             ),
-         Key([mod], "n",
-             lazy.layout.normalize(),
-             desc='normalize window size ratios'
-             ),
-         Key([mod], "m",
-             lazy.layout.maximize(),
-             desc='toggle window between minimum and maximum sizes'
-             ),
-         Key([mod, "shift"], "f",
-             lazy.window.toggle_floating(),
-             desc='toggle floating'
-             ),
-         Key([mod], "f",
-             lazy.window.toggle_fullscreen(),
-             desc='toggle fullscreen'
-             ),
-         ### Stack controls
-         Key([mod, "shift"], "Tab",
-             lazy.layout.rotate(),
-             lazy.layout.flip(),
-             desc='Switch which side main pane occupies (XmonadTall)'
-             ),
-          Key([mod], "space",
-             lazy.layout.next(),
-             desc='Switch window focus to other pane(s) of stack'
-             ),
-         Key([mod, "shift"], "space",
-             lazy.layout.toggle_split(),
-             desc='Toggle between split and unsplit sides of stack'
-             ),
-]
-
-layout_theme = {"border_width": 2,
-                "margin": 20,
-                "border_focus": zerotwo['bg'],
-                "border_normal": zerotwo['darkBg']
-                }
-
-layouts = [
-    MonadTall(**layout_theme)
-]
-
-widget_defaults = dict(
-    font = "sans",
-    fontsize = 10,
-    padding = 2,
-    foreground = zerotwo['fg'],
-    background = zerotwo['bg']
-)
-                         
-extension_defaults = widget_defaults.copy()
-
-screens = [Screen(top = bar)]
-
-groups = [
-    Group('1', label="HOME", layout="monadtall"),
-    Group(name='2', label="WEB", matches=[Match(wm_class='qutebrowser')], layout="monadtall"),
-    Group(name='3', label="DEV", matches=[Match(wm_class='Alacritty')], layout="monadtall"),
-    Group(name='4', label="CHAT", layout='monadtall')
-]
-
-for i in groups:
-    keys.extend([
-        # mod1 + letter of group = switch to group
-        Key([mod], i.name, lazy.group[i.name].toscreen(),
-            desc="Switch to group {}".format(i.name)),
-
-        # Or, use below if you prefer not to switch to that group.
-        # mod1 + shift + letter of group = move focused window to group
-        Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
-            desc="move focused window to group {}".format(i.name)),
-    ])
-    
-# Drag floating layouts.
-mouse = [
-Drag([mod], "Button1", lazy.window.set_position_floating(), start=lazy.window.get_position()),
-Drag([mod], "Button3", lazy.window.set_size_floating(), start=lazy.window.get_size()),
-Click([mod], "Button2", lazy.window.bring_to_front()),
-]
-
-dgroups_key_binder = None
-dgroups_app_rules = []  # type: list
-follow_mouse_focus = True
-bring_front_click = False
-cursor_warp = False
 auto_fullscreen = True
 focus_on_window_activation = "smart"
 reconfigure_screens = True
 auto_minimize = True
+wl_input_rules = None
 wmname = "LG3D"
-
-floating_layout = layout.Floating(
-    float_rules=[
-        # Run the utility of `xprop` to see the wm class and name of an X client.
-        *layout.Floating.default_float_rules,
-        Match(wm_class="confirmreset"),  # gitk
-        Match(wm_class="makebranch"),  # gitk
-        Match(wm_class="maketag"),  # gitk
-        Match(wm_class="ssh-askpass"),  # ssh-askpass
-        Match(title="branchdialog"),  # gitk
-        Match(title="pinentry"),  # GPG key password entry,
-        Match(wm_class="Pavucontrol"),
-        Match(wm_class="nitrogen")
-    ]
-)
 
 @hook.subscribe.startup_once
 def start_once():
