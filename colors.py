1ba9115d9062f984cfc3404efe44019a5b7391da
Ivan Gregor Tabalno <96023410+igmt-official@users.noreply.github.com>
2022-03-11 17:04:53 +0800

    Update colors.py

--- colors.py
+++ colors.py
@@ -1,6 +1,5 @@
-zerotwo = { "bg": "#f299a5",
-           "darkBg": "#e8849d",
-           "fg": "#ffffff",
-           "grey": "#7e4f54",
-           "darkGrey": "#4f3436",
-          }
+"bg" = "#f299a5"
+"darkBg" = "#e8849d"
+"fg" = "#ffffff"
+"grey" = "#7e4f54"
+"darkGrey" = "#4f3436"
