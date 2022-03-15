cdcfdcb3647dbd7e6704a0dd6537a8db4d4713a9
Ivan Gregor Tabalno <96023410+igmt-official@users.noreply.github.com>
2022-03-16 01:49:05 +0800

    Update widgets.py

--- .config/qtile/settings/widgets.py
+++ .config/qtile/settings/widgets.py
@@ -1,9 +1,7 @@
 from .colors import colors
 
 widget_defaults = dict(
-    font="Ubuntu Mono",
     fontsize = 12,
     padding = 2,
-    background = colors['bg']
 )
 extension_defaults = widget_defaults.copy()
