b36d572c964a060401cfbfd743b2ba75d705629f
Ivan Gregor Tabalno <96023410+igmt-official@users.noreply.github.com>
2022-03-15 10:22:24 +0800

    Update colors.py

--- .config/qtile/settings/colors.py
+++ .config/qtile/settings/colors.py
@@ -1,7 +1,10 @@
 colors = {
-    "bg": "#f299a5"
-    "darkBg": "#e8849d"
-    "fg": "#ffffff"
-    "grey": "#7e4f54"
-    "darkGrey": "#4f3436"
-}
+        "bg": "#883141",
+        "darkBg": "#672d3c",
+        "fg": "#ffffff",
+        "inactive": "#2c2a37",
+        "red": "#ff2e2e",
+        "active": "#bba1ac",
+        "white": "#ffffff",
+        "light_pink": "#bfb1be",
+        }
