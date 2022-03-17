65fc8953592bfc2237bcd71a208f29f22d551599
Ivan Gregor Tabalno <96023410+igmt-official@users.noreply.github.com>
2022-03-17 23:34:52 +0800

    Create init.lua

---
+++ .config/nvim/lua/options/init.lua
@@ -0,0 +1,27 @@
+local set = vim.opt
+
+-- set.shell = "/bin/sh"
+
+set.expandtab = true
+set.smarttab = true
+set.shiftwidth = 4
+set.tabstop = 4
+set.number = true
+set.hlsearch = true
+set.incsearch = true
+set.ignorecase = true
+set.smartcase = true
+set.termguicolors = true
+set.showmode = false
+set.splitbelow = true
+set.splitright = true
+set.wrap = false
+set.breakindent = true
+set.scrolloff = 5
+set.fileencoding = "utf-8"
+set.conceallevel = 2
+set.swapfile = false
+set.mouse = "a"
+set.smartindent = true
+
+vim.notify = require("notify")
