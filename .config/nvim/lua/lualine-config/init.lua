167bf265215ffa52a2e970f506f20fe5f82b9ec8
Ivan Gregor Tabalno <96023410+igmt-official@users.noreply.github.com>
2022-03-17 23:33:57 +0800

    Create init.lua

---
+++ .config/nvim/lua/lualine-config/init.lua
@@ -0,0 +1,7 @@
+require('lualine').setup{
+    options = {
+        theme = 'dracula-nvim',
+        component_separators = { left = '', right = ''},
+        section_separators = { left = '', right = ''},
+    },
+}
