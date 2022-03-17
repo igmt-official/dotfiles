a1bd82ff79e02f6d33ff7f37d7bf1f8cdef7a49b
Ivan Gregor Tabalno <96023410+igmt-official@users.noreply.github.com>
2022-03-17 23:29:57 +0800

    Create init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,1 +1,10 @@
-
+require "options"
+require "keymap-config"
+-- require "packer-config"
+-- require "colorschemes-config.dracula"
+-- require "nvim-tree-config"
+-- require "lsp-config.language-servers"
+-- require "lsp-config.nvim-cmp"
+-- require "lualine-config"
+-- require "barbar-config"
+-- require "treesitter-config"
