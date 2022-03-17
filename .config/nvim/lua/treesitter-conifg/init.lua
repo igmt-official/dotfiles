bfd5c58be923be648f23867c9db2598eb01bd233
Ivan Gregor Tabalno <96023410+igmt-official@users.noreply.github.com>
2022-03-17 23:37:02 +0800

    Create init.lua

---
+++ .config/nvim/lua/treesitter-conifg/init.lua
@@ -0,0 +1,16 @@
+--> Neorg parser configs <--
+-- local parser_configs = require("nvim-treesitter.parsers").get_parser_configs()
+
+-- require("nvim-treesitter.configs").setup({
+-- 	--> parsers <--
+-- 	ensure_installed = {
+-- 		"python",
+-- 		"lua",
+-- 	},
+-- 	sync_install = false,
+-- 	highlight = {
+-- 		enable = true,
+-- 		additional_vim_regex_highlighting = false,
+-- 	},
+	
+-- })
