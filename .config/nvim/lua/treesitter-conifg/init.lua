1152a28b01502a3e5f7e729f7cafa4a92ef64a0a
Ivan Gregor Tabalno <96023410+igmt-official@users.noreply.github.com>
2022-03-17 23:40:04 +0800

    Update init.lua

--- .config/nvim/lua/treesitter-conifg/init.lua
+++ .config/nvim/lua/treesitter-conifg/init.lua
@@ -1,16 +1,28 @@
---> Neorg parser configs <--
--- local parser_configs = require("nvim-treesitter.parsers").get_parser_configs()
-
 -- require("nvim-treesitter.configs").setup({
 -- 	--> parsers <--
 -- 	ensure_installed = {
 -- 		"python",
 -- 		"lua",
 -- 	},
+
 -- 	sync_install = false,
 -- 	highlight = {
 -- 		enable = true,
 -- 		additional_vim_regex_highlighting = false,
+-- 	},
+
+--     indent = {
+-- 		enable = true,
+-- 	},
+
+--     --> refactor module
+-- 	refactor = {
+-- 		smart_rename = {
+-- 			enable = true,
+-- 			keymaps = {
+-- 				smart_rename = "grr",
+-- 			},
+-- 		},
 -- 	},
 	
 -- })
