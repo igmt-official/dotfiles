310839581bd3d0722323312495ec82984fa239f2
Ivan Gregor Tabalno <96023410+igmt-official@users.noreply.github.com>
2022-03-17 23:35:13 +0800

    Create init.lua

---
+++ .config/nvim/lua/packer-config/init.lua
@@ -0,0 +1,28 @@
+return require("packer").startup(function()
+    -- Plugin Manager
+	use'wbthomason/packer.nvim'
+
+    -- Colorschemes
+    use 'Mofiqul/dracula.nvim'
+
+    -- Nvim Tree
+    use 'kyazdani42/nvim-tree.lua'
+    use 'kyazdani42/nvim-web-devicons'
+
+    -- Treesitter
+    use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
+
+    -- Lsp
+    use 'neovim/nvim-lspconfig'
+    use 'hrsh7th/nvim-cmp' -- Autocompletion plugin
+    use 'hrsh7th/cmp-nvim-lsp' -- LSP source for nvim-cmp
+    use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
+    use 'L3MON4D3/LuaSnip' -- Snippets plugin
+    use 'onsails/lspkind-nvim'
+
+    use 'rcarriga/nvim-notify'
+    use 'nvim-lualine/lualine.nvim'
+    use 'romgrk/barbar.nvim'
+
+end)
+
