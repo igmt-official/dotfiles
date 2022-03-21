2fe44f4ada06d025002bdb3244b7b41c0d91a26f
igmt-official <igmtofficial@gmail.com>
2022-03-21 10:41:59 +0800

    Updated.

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -1,9 +1,9 @@
-import dracula.draw
+import aliScheme.draw
 
 # Load existing settings made via :set
 config.load_autoconfig()
 
-dracula.draw.blood(c, {
+aliScheme.draw.blood(c, {
     'spacing': {
         'vertical': 6,
         'horizontal': 8
